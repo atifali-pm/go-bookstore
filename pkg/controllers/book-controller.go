b1c39670c3f351b58efa5063a6a5b883191f338e
Atif <atif@monotote.com>
2023-06-20 11:01:39 +0500

    New changes

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -13,7 +13,7 @@ import(
 var NewBook models.Book
 
 func GetBook(w http.ResponseWriter, r *http.Request){
-	newBooks:=models.GetAllBooks()
+	newBooks := models.GetAllBooks()
 	res, _ :=json.Marshal(newBooks)
 	w.Header().Set("Content-Type","pkglication/json")
 	w.WriteHeader(http.StatusOK)
